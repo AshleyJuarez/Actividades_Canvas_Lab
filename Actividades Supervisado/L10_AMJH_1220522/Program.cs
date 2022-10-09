561dd14ce44a00c57a7e6b7d3d064fd849a0f710
AshleyJuarez <111013416+AshleyJuarez@users.noreply.github.com>
2022-10-09 15:58:15 -0600

    Subiendo actividades trabajo supervisado

---
+++ Actividades Supervisado/L10_AMJH_1220522/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10_AMJH_1220522
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LAB TS 10");
+            Console.WriteLine("Ingrese radio del círculo");
+            double radioIngresado = double.Parse(Console.ReadLine());
+
+            //Declaración de variables para almacenar los cálculos
+            double areaCalculada = 0;
+            double perimetroCalculado = 0;
+            double volumenCalculado = 0;
+
+            //Objeto de tipo Círculo
+            Circulo objCirculo = new Circulo(radioIngresado);
+
+            //Guardar los cálculos
+            objCirculo.CalcularGeometria(ref perimetroCalculado, ref areaCalculada, ref volumenCalculado);
+
+            Console.WriteLine("Perimetro: " + perimetroCalculado);
+            Console.WriteLine("Area: " + areaCalculada);
+            Console.WriteLine("Volumen: " + volumenCalculado);
+
+            Console.ReadKey();
+
+            Console.WriteLine("");
+            //Solicitar cateto y angulo opuesto del triangulo
+            Console.WriteLine("Ingrese longitud del cateto del triángulo: ");
+            double catetoA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los grados del ángulo opuesto del triángulo: ");
+            double anguloOpuestoA = double.Parse(Console.ReadLine());
+
+            TrianguloRectangulo objTriangulo = new TrianguloRectangulo(catetoA, anguloOpuestoA);
+
+            //Guardar los cálculos
+            double catetoA_ = objTriangulo.ObtenerCatetoA();
+            double catetoB = objTriangulo.ObtenerCatetoB();
+            double hipotenusa = objTriangulo.ObtenerHipotenusa();
+            double anguloOpuestoA_ = objTriangulo.ObtenerAnguloOpuestoA();
+            double anguloOpuestoB = objTriangulo.ObtenerAnguloOpuestoB();
+            double area = objTriangulo.ObtenerArea();
+
+            //Mostrar datos
+            Console.WriteLine("Valor de cateto A: " + catetoA_);
+            Console.WriteLine("Valor de cateto B: " + catetoB);
+            Console.WriteLine("Valor de hipotenusa: " + hipotenusa);
+            Console.WriteLine("Valor angulo opuesto A: " + anguloOpuestoA_);
+            Console.WriteLine("Valor angulo opuesto B: " + anguloOpuestoB);
+            Console.WriteLine("Valor de área: " + area);
+
+            Console.ReadKey();
+
+        }
+    }
+    class Circulo
+    {
+        private double radio;
+        public Circulo (double Radio)
+        {
+            radio = Radio;
+        }
+        private double ObtenerPerimetro()
+        {
+            double perimetro = 2 * Math.PI * radio;
+            return perimetro;
+        }
+        private double ObtenerArea()
+        {
+            double area = Math.PI * Math.Pow(radio, 2);
+            return area;
+        }
+        private double ObtenerVolumen()
+        {
+            double volumen = (4 * Math.PI * Math.Pow(radio, 3)) / 3;
+            return volumen;
+        }
+        public void CalcularGeometria(ref double unPerimetro, ref double unArea, ref double unVolumen)
+        {
+            unPerimetro = ObtenerPerimetro();
+            unArea = ObtenerArea();
+            unVolumen = ObtenerVolumen();
+        }
+        
+    }
+
+    class TrianguloRectangulo 
+    {
+        private double catetoA;
+        private double anguloOpuestoA;
+
+        public TrianguloRectangulo(double CatetoA, double AnguloOpuestoA) 
+        {
+            catetoA = CatetoA;
+            anguloOpuestoA = AnguloOpuestoA;
+        }
+
+        public double ObtenerCatetoA()
+        {
+            return Math.Round(catetoA, 3);
+        }
+
+        public double ObtenerCatetoB() 
+        {
+            double angulo = anguloOpuestoA * Math.PI / 180;
+            double catetoB = catetoA / Math.Tan(angulo);
+            return Math.Round(catetoB,3);
+        }
+
+        public double ObtenerHipotenusa() 
+        {
+            double angulo = anguloOpuestoA * Math.PI / 180;
+            double hipotenusa = ObtenerCatetoB() / Math.Cos(angulo);
+            return Math.Round(hipotenusa, 3);
+        }
+
+        public double ObtenerAnguloOpuestoA()
+        {
+            return Math.Round(anguloOpuestoA, 3);
+        }
+
+        public double ObtenerAnguloOpuestoB() 
+        {
+            double anguloOpuestoB = 90 - anguloOpuestoA;
+            return Math.Round(anguloOpuestoB, 3);
+        }
+
+        public double ObtenerArea() 
+        {
+            double area = (catetoA * ObtenerCatetoB()) / 2;
+            return Math.Round(area, 3);
+        }
+
+    }
+    
+}
---
+++ Actividades Supervisado/T6_AMJH_1220522/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6_AMJH_1220522
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tarea Laboratorio No. 6");
+            Console.WriteLine("Signo zodiacal");
+            Console.WriteLine("¿Cuál es el año de su nacimiento?");
+            int año = int.Parse(Console.ReadLine());
+            Console.WriteLine("¿Cuál es el mes de su nacimiento?");
+            string mes = Console.ReadLine();
+            Console.WriteLine("¿Cuál es el día de su nacimiento?");
+            int dia = int.Parse(Console.ReadLine());
+
+            //mes = mes.ToUpper();
+
+            if ((dia > 0) && (dia <= 31))
+            {
+                if (mes.Equals("marzo"))
+                {
+                    if (dia >= 21)
+                        Console.WriteLine("Su signo es: Aries");
+                    else
+                        Console.WriteLine("Su signo es: Piscis");
+                }
+                else if (mes.Equals("abril"))
+                {
+                    if (dia >= 20)
+                        Console.WriteLine("Su signo es: Tauro");
+                    else
+                        Console.WriteLine("Su signo es: Aries");
+                }
+                else if (mes.Equals("mayo"))
+                {
+                    if (dia >= 21)
+                        Console.WriteLine("Su signo es: Géminis");
+                    else
+                        Console.WriteLine("Su signo es: Tauro");
+                }
+                else if (mes.Equals("junio"))
+                {
+                    if (dia >= 21)
+                        Console.WriteLine("Su signo es: Cancer");
+                    else
+                        Console.WriteLine("Su signo es: Géminis");
+                }
+                else if (mes.Equals("julio"))
+                {
+                    if (dia >= 23)
+                        Console.WriteLine("Su signo es: Leo");
+                    else
+                        Console.WriteLine("Su signo es: Cancer");
+                }
+                else if (mes.Equals("Agosto"))
+                {
+                    if (dia >= 23)
+                        Console.WriteLine("Su signo es: Virgo");
+                    else
+                        Console.WriteLine("Su signo es: Leo");
+                }
+                else if (mes.Equals("septiembre"))
+                {
+                    if (dia >= 23)
+                        Console.WriteLine("Su signo es: Libra");
+                    else
+                        Console.WriteLine("Su signo es: Virgo");
+                }
+                else if (mes.Equals("octubre"))
+                {
+                    if (dia >= 21)
+                        Console.WriteLine("Su signo es: Escorpio");
+                    else
+                        Console.WriteLine("Su signo es: Libra");
+                }
+                else if (mes.Equals("Novimebre"))
+                {
+                    if (dia >= 22)
+                        Console.WriteLine("Su signo es: Sagitario");
+                    else
+                        Console.WriteLine("Su signo es: Escorpio");
+                }
+                else if (mes.Equals("diciembre"))
+                {
+                    if (dia >= 22)
+                        Console.WriteLine("Su signo es: Capricornio");
+                    else
+                        Console.WriteLine("Su signo es: Sagitario");
+                }
+                else if (mes.Equals("enero"))
+                {
+                    if (dia >= 20)
+                        Console.WriteLine("Su signo es: Acuario");
+                    else
+                        Console.WriteLine("Su signo es: Capricornio");
+                }
+                else if (mes.Equals("febrero"))
+                {
+                    if (dia >= 21)
+                        Console.WriteLine("Su signo es: Piscis");
+                    else
+                        Console.WriteLine("Su signo es: Acuario");
+                }
+            }
+            else
+                Console.WriteLine("El día debe ser mayor a 0 y menor a 31");
+                        
+            {
+                Console.WriteLine("Hecho por: Ashley Juárez");
+                Console.WriteLine("El programa ha terminado");
+                Console.ReadKey();
+            }
+        }
+    }
+}
